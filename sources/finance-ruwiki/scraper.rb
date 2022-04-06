59bc6b5d36aa20d827017b611aeb73b620192487
Tony Bowden <tony@tmtm.com>
2022-04-06 17:33:10 +0300

    update scraper for latest EPS

--- sources/finance-ruwiki/scraper.rb
+++ sources/finance-ruwiki/scraper.rb
@@ -4,45 +4,6 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-# Russian dates
-class RussianExtd < WikipediaDate
-  REMAP = {
-    'Настоящее время' => '',
-    'настоящее время' => '',
-    'января'          => 'January',
-    'январь'          => 'January',
-    'февраля'         => 'February',
-    'февраль'         => 'February',
-    'марта'           => 'March',
-    'апреля'          => 'April',
-    'апрель'          => 'April',
-    'мая'             => 'May',
-    'май'             => 'May',
-    'июня'            => 'June',
-    'июнь'            => 'June',
-    'июля'            => 'July',
-    'августа'         => 'August',
-    'сентября'        => 'September',
-    'сентябрь'        => 'September',
-    'октября'         => 'October',
-    'октябрь'         => 'October',
-    'октяябрь'        => 'October',
-    'ноября'          => 'November',
-    'ноябрь'          => 'November',
-    'декабря'         => 'December',
-    'декабрь'         => 'December',
-  }.freeze
-
-  def remap
-    super.merge(REMAP)
-  end
-
-  def date_str
-    super.to_s
-  end
-end
-
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
   decorator UnspanAllTables
@@ -66,12 +27,16 @@ class OfficeholderList < OfficeholderListBase
       noko.xpath('text()').text.gsub(' с ', '').gsub(' года', '').split(/[—-]/).map(&:tidy).reject(&:empty?)
     end
 
+    def raw_end
+      super.to_s
+    end
+
     def name_cell
       noko.css('a')
     end
 
-    def date_class
-      RussianExtd
+    def empty?
+      false
     end
   end
 end
